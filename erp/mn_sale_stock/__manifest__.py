62b5c258fb6400dc821475a70b588138d215fd4f
zolboo <zolboo.o@asterisk-tech.mn>
2025-04-21 14:04:45 +0800

    [IMP] Эхний commit

---
+++ erp/mn_sale_stock/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Mongolian Sale Stock',
+    'version': '17.0',
+    'category': 'Mongolian Modules',
+    'summary': 'This module allows you to smartly manage sale stock.',
+    'description': 'Sale Stock.',
+    'author': 'Z',
+    'company': 'Z',
+    'maintainer': 'Z',
+    'website': '',
+    'depends': ['sale_stock'],
+    "data": [
+    ],
+    'license': 'GPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
